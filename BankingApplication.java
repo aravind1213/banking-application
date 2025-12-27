305b8cddfc3fc107e98101de92888c0bfa4cd725
Ayodhya Aravind <ayodhyaaravind@gmail.com>
2025-12-27 10:50:24 +0530

    Banking Application

--- BankingApplication.java
+++ BankingApplication.java
@@ -55,26 +55,31 @@ public static void main(String[] args) {
 	System.out.println("Enter initial balence in your account :");
 	double balence=sc.nextDouble();
 	BankingApplication obj=new BankingApplication(customerName,customerAddress,phoneNumber,balence);
+	int operation=0;
+	do {
 	System.out.println("What do you want to do");
 	System.out.println("Enter 1 for deposit");
 	System.out.println("Enter 2 for withdraw");
 	System.out.println("Enter 3 to check balence");
 	System.out.println("Enter 4 to exit");
-	int operation=sc.nextInt();
+	operation=sc.nextInt();
 	switch(operation)
 	{
 	case 1:
 		System.out.println("Enter amount to deposit :");
 		int amount=sc.nextInt();
 		obj.deposit(amount);
+		break;
 		
 	case 2:
 		System.out.println("Enter amount to withdraw :");
 		amount=sc.nextInt();
 		obj.withdraw(amount);
+		break;
 		
 	case 3:
 		obj.showBalance();
+		break;
 		
 	case 4:
 		obj.exit();
@@ -82,7 +87,8 @@ public static void main(String[] args) {
 		
 	default:
 		System.out.println("Enter correct input");
-	}
+	}}while(operation!=4);
+	
 	sc.close();
 		
 		
